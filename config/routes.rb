a4f7d02c9351d1be50074696bd8b20865ac26df8
= <=>
2021-04-25 10:15:44 +0530

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :urls
-  root 'pages#index'
-  get  "/report", to: "pages#index"
+  root 'home#index'
+  get  "/report", to: "home#index"
   get '/:id' => "urls#redirect" 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
